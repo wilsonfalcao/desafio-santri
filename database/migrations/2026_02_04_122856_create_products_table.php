a0a0d94124b54a99071db291efb3d28c2c7d7f07
Wilson Falcão <wilson.falcao1@hotmail.com>
2026-02-04 10:10:37 -0300

    feat(Migration): include products migration

---
+++ database/migrations/2026_02_04_122856_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->string('sku')->nullable();
+            $table->decimal('regular_price', 10, 4)->nullable('000000.0000');
+            $table->decimal('price', 10, 4)->nullable('000000.0000');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
