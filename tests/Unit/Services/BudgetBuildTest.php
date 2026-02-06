4247a0c934b312f183d55ffdf33528d5589e80b2
Wilson Falcão <wilson.falcao1@hotmail.com>
2026-02-06 12:51:26 -0300

    rafactor(test): update budget build test

--- tests/Unit/Services/BudgetBuildTest.php
+++ tests/Unit/Services/BudgetBuildTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Services;
 
 use App\Models\BudgetBuild;
 use App\Models\Product;
+use App\Models\User;
 use Mockery;
 use PHPUnit\Framework\TestCase;
 
@@ -12,22 +13,24 @@ class BudgetBuildTest extends TestCase
     /**
      * A basic unit test example.
      */
+
+    private array $arrayJson = [
+        'id' => 'teste',
+        'product' => [
+            ['id' => 1, 'quantity' => 2]
+        ],
+        'user_id' => 1
+    ];
+
     public function test_build_from_json_static_function_correctly(): void
     {
-        $arrayJson = [
-            'id' => 'teste',
-            'product' => [
-                ['id' => 1, 'quantity' => 2]
-            ],
-            'user_id' => 1
-        ];
 
-        $budgetMock = BudgetBuild::fromJson($arrayJson);
+        $budgetMock = BudgetBuild::fromJson($this->arrayJson);
 
         $this->assertInstanceOf(BudgetBuild::class, $budgetMock);
     }
 
-    public function test_product_access_from_budget_build_behavior_correctly(): void
+    public function test_product_access_for_budget_build_correctly(): void
     {
 
         $productMock = Mockery::mock('alias:App\Models\Product');
@@ -36,17 +39,24 @@ class BudgetBuildTest extends TestCase
             ->with(1)
             ->andReturn($productMock);
 
-        $arrayJson = [
-            'id' => 'teste',
-            'product' => [
-                ['id' => 1, 'quantity' => 2]
-            ],
-            'user_id' => 1
-        ];
-
-        $budgetMock = BudgetBuild::fromJson($arrayJson);
+        $budgetMock = BudgetBuild::fromJson($this->arrayJson);
         $result = $budgetMock->getProduct();
 
         $this->assertInstanceOf(Product::class, $result);
+    }
+
+    public function test_user_access_for_budget_build_correctly(): void
+    {
+
+        $productMock = Mockery::mock('alias:App\Models\User');
+
+        $productMock->shouldReceive('find')
+            ->with(1)
+            ->andReturn($productMock);
+
+        $budgetMock = BudgetBuild::fromJson($this->arrayJson);
+        $result = $budgetMock->getUser();
+
+        $this->assertInstanceOf(User::class, $result);
     }
 }
